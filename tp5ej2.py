a423d93b1e78af3dc592812c64c60e947ed65d70
agustin-montiveros <agus.monti14@outlook.com>
2021-05-30 04:15:19 -0300

    Punto 2 terminado

--- tp5ej2.py
+++ tp5ej2.py
@@ -1,15 +1,23 @@
 ################
-# Martín René - @martinvilu
+# Agustin Montiveros - @agustin-montiveros
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
 
 # Reemplazar por las funciones del ejercicio
 
+def fibonacci(n):
+    a= 0
+    b= 1
+    while a < n:       
+        print(a, end=",")
+        a, b = b, a+b
+    
 
 def prueba():
-    """Toda la interacción con el usuario va acá"""
-    pass
+        n= int(input("Ingrese el numero limite de fibonacci: "))
+        print(fibonacci(n))
+        pass
 
 if __name__ == "__main__":
     prueba()
