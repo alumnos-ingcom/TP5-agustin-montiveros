5ec4557873009c8a5873e2b944ad1c37c17e344a
agustin-montiveros <agus.monti14@outlook.com>
2021-06-04 03:48:51 -0300

    Punto 5 corregido

--- tp5ej5.py
+++ tp5ej5.py
@@ -6,13 +6,18 @@
 
 # Reemplazar por las funciones del ejercicio
 
-def revertirCaracteres(texto):
-    texto_cambiado = texto.swapcase()
-    return texto_cambiado
+def revertir_caracteres(texto):
+    invertido = ""  #se crea una variable con un cadena string vacia
+    for c in texto:  #por cada caracter en el texto introducido
+        if c.isupper() == True:     #si el caracter es Mayuscula
+            invertido = invertido + c.lower()  #se invierte ese caracter a Minuscula y se suma a la cadena string
+        else:  #si el caracter no es Mayuscula, por ende es Minuscula
+            invertido = invertido + c.upper()  #se invierte ese caracter a Mayuscula y se suma a la cadena string
+    return invertido  
 
 def prueba():
     texto = str(input("Ingresar texto: "))
-    resultado = revertirCaracteres(texto)
+    resultado = revertir_caracteres(texto)
     print(resultado)
 
 if __name__ == "__main__":
