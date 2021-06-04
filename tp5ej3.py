9513343df2e585ffc00b43b874123ffff1574afe
agustin-montiveros <agus.monti14@outlook.com>
2021-06-04 03:31:25 -0300

    Punto 3 corregido

--- tp5ej3.py
+++ tp5ej3.py
@@ -7,23 +7,22 @@
 # Reemplazar por las funciones del ejercicio
 
 
-def tribonacci(n):
-    a= 0
-    b= 0
-    c= 1
-    d= 0
-    while a < n:       
-        print(a, end=",")
-        d = a + b + c
-        a = b
-        b = c
-        c = d
-    
+def tribonacci(numero):  # n = numero para la explicacion
+    lista = [1,1,1] #los primeros 3 numeros de la lista son 1, dado que segun la formula a(n): a(0)=a(1)=a(2)=1.
+    if numero > 3: #Si el numero ingresado es mayor a 3, porque segun la formula a(n): a(0)=a(1)=a(2)=1. 
+        for i in range(numero-3): #el "-3" es para que nos cuente los 3 primeros "1"
+            valor_a= sum(lista[-3:]) #se suma los ultimos 3 numeros de la lista
+            lista.append(valor_a) #se agrega el resultado de la suma de los 3 ultimos numeros de la lista a su ultimo lugar
+        return lista
+    else:
+        return lista[:numero] #si numero no es mayor a 3, retornamos la lista original desde el principio hasta el numero ingresado
+            
 
 def prueba():
-        n= int(input("Ingrese el numero limite de fibonacci: "))
-        print(tribonacci(n))
-        pass
+        numero= int(input("Ingrese el numero limite de fibonacci: "))
+        resultado = tribonacci(numero)
+        print(resultado)
+
     
     
 if __name__ == "__main__":
