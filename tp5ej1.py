fcbd031b97098879bbafcd604183af02f94934de
agustin-montiveros <agus.monti14@outlook.com>
2021-06-04 02:05:20 -0300

    Punto 1 corregido

--- tp5ej1.py
+++ tp5ej1.py
@@ -6,11 +6,10 @@
 
 # Reemplazar por las funciones del ejercicio
 
-def parOimpar(numero):
+def par_o_impar(numero):
     for i in range(numero): #aca ponemos un contador con el numero ingresado como limite
         mitad = numero / 2  #aca obtenemos la mitad del numero ingresado
-        cero = mitad - int(mitad) #al pasar a entero descartamos la parte decimal, y hacemos la resta
-        print(cero)   
+        cero = mitad - int(mitad) #al pasar a entero descartamos la parte decimal, y hacemos la resta   
         if cero == 0:   #si la resta da 0, es par, si da otro numero es impar.
             return True
         else:
@@ -22,10 +21,10 @@ def parOimpar(numero):
 
 def prueba():
     numero = int(input("Insertar numero: "))
-    print(parOimpar(numero))
+    resultado = par_o_impar(numero)
+    print("El numero es par?: ", resultado)
         
         
-    pass
 
 if __name__ == "__main__":
     prueba()
